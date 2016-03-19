d3e33b6c67e8eba85dd86969af408abc2583f67f
Leo <leo.l.huang@hotmail.com>
2016-03-18 22:51:00 -0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function myFunction() {
    	document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }  
+
+function hideall()
+{
+    document.getElementById(about).className = "hide";
+    document.getElementById(contact).className = "hide";
+
+}
+
+function about(){
+
+	document.getElementById(about).className = "show";
+	document.getElementById(contact).className = "hide";
+
+}
