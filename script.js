4bb35235f0fc40c19f1d1c63b9437455816ea547
Leo <leo.l.huang@hotmail.com>
2016-03-19 12:25:14 -0700

    media update
    js update to hide and show text when clicking on different menu items.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function myFunction() {
+function myFunction(){
    	document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }  
 
-function hideall()
-{
-    document.getElementById(about).className = "hide";
-    document.getElementById(contact).className = "hide";
-
+function hideall(){
+    document.getElementById("about").className = "hide";
+    document.getElementById("contact").className = "hide";
+    document.getElementById("media").className = "hide";
 }
 
 function about(){
-
-	document.getElementById(about).className = "show";
-	document.getElementById(contact).className = "hide";
-
+	document.getElementById("about").className = "show";
+	document.getElementById("media").className = "hide";
 }
+
+function media(){
+	document.getElementById("media").className = "show";
+	document.getElementById("about").className = "hide";
+}
+
