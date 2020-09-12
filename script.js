a0686781c1b632a221619407f38d604f9f80ae58
leo.l.huang@hotmail.com <Leo.L.Huang@hotmail.com>
2020-09-12 10:43:29 -0700

    Changed hiding of title, and modified pages

--- script.js
+++ script.js
@@ -4,28 +4,29 @@ function myFunction() {
 
 function hideall() {
   $('#social, #contact, #about, #projects').fadeOut(300);
+  $('#title').delay(400).fadeIn(300);
 }
 
 function about() {
         
-  $('#social, #contact, #projects').fadeOut(300);
+  $('#social, #contact, #projects, #title').fadeOut(300);
   $('#about').delay(400).fadeIn(300);
 
 }
 
 function contact() {
 
-  $('#social, #about, #projects').fadeOut(300);
+  $('#social, #about, #projects, #title').fadeOut(300);
   $('#contact').delay(400).fadeIn(300);
 
 }
 
 function social() {
-  $('#about, #contact, #projects').fadeOut(300);
+  $('#about, #contact, #projects, #title').fadeOut(300);
   $('#social').delay(400).fadeIn(300);
 }
 
 function projects() {
-   $('#about, #contact, #social').fadeOut(300);
+   $('#about, #contact, #social, #title').fadeOut(300);
    $('#projects').delay(400).fadeIn(300);
 }
