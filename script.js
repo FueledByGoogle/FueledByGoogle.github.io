080964725a2cb14d297f53b161104757e21ddf6e
Leo Huang <Leo.L.Huang@hotmail.com>
2021-07-25 01:29:53 -0700

    Cleaned up and updated code

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-function myFunction() {
+function openMenu() {
   document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }  
 
 function hideall() {
-  $('#social, #contact, #about, #projects').fadeOut(300);
+  $('#contact, #about, #projects').fadeOut(300);
   $('#title').delay(400).fadeIn(300);
 }
 
 function about() {
         
-  $('#social, #contact, #projects, #title').fadeOut(300);
+  $('#contact, #projects, #title').fadeOut(300);
   $('#about').delay(400).fadeIn(300);
 
 }
 
 function contact() {
 
-  $('#social, #about, #projects, #title').fadeOut(300);
+  $('#about, #projects, #title').fadeOut(300);
   $('#contact').delay(400).fadeIn(300);
 
 }
 
-function social() {
-  $('#about, #contact, #projects, #title').fadeOut(300);
-  $('#social').delay(400).fadeIn(300);
-}
-
 function projects() {
-   $('#about, #contact, #social, #title').fadeOut(300);
+   $('#about, #contact, #title').fadeOut(300);
    $('#projects').delay(400).fadeIn(300);
 }
